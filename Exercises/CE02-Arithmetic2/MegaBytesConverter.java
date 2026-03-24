8600d2725eb13c96ee3b0152cd81dd80b70cde29
Dwima Basumatary <dwima1583@gmail.com>
2026-03-24 20:59:05 +0530

    ce3

---
+++ Exercises/CE02-Arithmetic2/MegaBytesConverter.java
@@ -0,0 +1,12 @@
+public class MegaBytesConverter {
+    public static void printMegaBytesAndKiloBytes(int kiloBytes){
+        if(kiloBytes < 0){
+            System.out.println("Invalid Value");
+            return;
+        }
+        int megaByte = kiloBytes/1024;
+        int remainingKiloByte = kiloBytes % 1024;
+        
+        System.out.println(kiloBytes + " KB = " +  megaByte + " MB and " + remainingKiloByte + " KB");
+    }
+}
